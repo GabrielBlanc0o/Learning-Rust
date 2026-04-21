dce8df61fd8bafc70e9d8193f0d295b66a4c2540
GabrielBlancoDev <gtbvacation@outlook.es>
2026-04-21 09:22:32 -0500

    Delete src directory

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-mod variables;
-mod numero_random;
-
-fn main() {
-   // println!("Hello, world!!!");
-    //print!("My name is Gabriel!! \n");
-    //variables::dos();
-    numero_random::numero_aleatorio();
-}
